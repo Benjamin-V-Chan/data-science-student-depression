21c92980292c33f96592f7b804d6d808acab0cb1
Benjamin Chan <benjamin.v.chan@gmail.com>
2025-01-11 00:46:52 -0800

    03 visualization script outline

---
+++ scripts/03_visualization.py
@@ -0,0 +1,5 @@
+# Load the cleaned dataset
+# Create specific plots:
+    # Distribution of depression by gender
+    # Impact of sleep duration on depression
+# Save visualizations to the 'outputs/' folder
